d5c064cd3b2064c877c7627fe850573dffab4830
Kulbhushan <kulbhushan.mayer@gmail.com>
2022-08-02 11:07:36 +0530

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $2 > message.txt
-curl $1 -X post -H `Content-type: application/json' --data "@message.txt"
+curl $1 -X post -H 'Content-type: application/json' --data "@message.txt"
